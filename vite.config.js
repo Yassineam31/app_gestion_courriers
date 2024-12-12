930337ac9a8168c4650f1e74c690c1db5df958d4
yassineam31 <yassineam31@gmail.com>
2024-12-12 11:49:17 +0100

    install breeze

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/dashboard.css',
-                'resources/css/sideBar.css',
-                'resources/css/footer.css',
-                'resources/css/courrierIndex.css',
-                'resources/css/formEntrant.css',
-                'resources/css/formSortant.css',
-                'resources/css/show.css',
                 'resources/js/app.js',
-                'resources/js/sideBar.js',
-                'resources/js/alerts.js',
-                
-                ],
+            ],
             refresh: true,
         }),
     ],
